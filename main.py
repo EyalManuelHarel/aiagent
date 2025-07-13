350ca2d82c5367cbaa70b59fda8ba2c006909f7f
Eyal Manuel Harel <eyalharelm@gmail.com>
2025-07-13 21:12:30 +0300

    trying 2

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def main():
     ]
     response = client.models.generate_content(model="gemini-2.0-flash-001",
                                               contents=messages)
-    print(response.text)
+    print(response.text) 
     
     if len(sys.argv) == 3 and sys.argv[2] == "--verbose":
         print(f"User prompt: {prompt}\nPrompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}")
