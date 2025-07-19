b22f2f6f1453eb3b9cd28b6bb290695fee291991
Eyal Manuel Harel <eyalharelm@gmail.com>
2025-07-19 17:18:27 +0300

    Adding functionality to agent

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,13 +1,10 @@
 import os
-import subprocess
-import sys
-from agent_config import CHAR_READ_LIMIT
+from google.genai import types
 
-def get_files_info(working_directory, directory=None):
+
+def get_files_info(working_directory, directory="."):
     abs_working_dir = os.path.abspath(working_directory)
-    target_dir = abs_working_dir
-    if directory:
-        target_dir = os.path.abspath(os.path.join(working_directory, directory))
+    target_dir = os.path.abspath(os.path.join(working_directory, directory))
     if not target_dir.startswith(abs_working_dir):
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
     if not os.path.isdir(target_dir):
@@ -25,55 +22,18 @@ def get_files_info(working_directory, directory=None):
         return "\n".join(files_info)
     except Exception as e:
         return f"Error listing files: {e}"
-    
-
-def get_file_content(working_directory, file_path):
-    if "Cannot list" in get_files_info(working_directory, file_path):
-        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
-    if os.path.isdir(file_path):
-        return f'Error: File not found or is not a regular file: "{file_path}"'
-    
-    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
-    try:
-        with open(abs_file_path, 'r', encoding='utf-8') as f:
-            content = f.read(CHAR_READ_LIMIT)
-            if f.read(1):  # Check if there is more content after 10000 chars
-                content += f'\n[...File "{file_path}" truncated at 10000 characters]'
-            return content
-    except Exception as e:
-        return f"Error: can't read file {e}"
-    
-
-def write_file(working_directory, file_path, content):
-    if "Cannot list" in get_files_info(working_directory, file_path):
-        return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
-    
-    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
-    try:
-        with open(abs_file_path, 'w', encoding='utf-8') as f:
-            result = f.write(content)
-            if result > 0:
-                return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
-            return "Error: can't write to file"
-    except Exception as e:
-        return f"Error: can't write to file {e}"
 
 
-def run_python_file(working_directory, file_path):
-    if "Cannot list" in get_files_info(working_directory, file_path):
-        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
-    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
-    if not os.path.exists(abs_file_path):
-        return f'Error: File "{file_path}" not found.'
-    if os.path.splitext(abs_file_path)[1] != ".py":
-        return f'Error: "{file_path}" is not a Python file.'
-    try:
-        result = subprocess.run([sys.executable, abs_file_path], stdout=True, stderr=True, cwd=working_directory, timeout=30)
-        return_str = f'STDOUT: {result.stdout}\nSTDERR: {result.stderr}'
-        if result.stdout == None:
-            return_str += "\nNo output produced."
-        if result.returncode != 0:
-            return_str += f'\nProcess exited with code {result.returncode}'
-        return return_str
-    except Exception as e:
-        return f"Error: executing Python file: {e}"
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+            ),
+        },
+    ),
+)
--- main.py
+++ main.py
@@ -4,26 +4,66 @@ from google import genai
 from google.genai import types
 import sys
 from agent_config import SYSTEM_PROMPT
+from call_function import available_functions, call_function
 
 def main():
     load_dotenv()
+
+    verbose = "--verbose" in sys.argv
+    args = []
+    for arg in sys.argv[1:]:
+        if not arg.startswith("--"):
+            args.append(arg)
+    if not args:
+        print("***AI Code Assistant***")
+        print('\nUsage: python main.py "your prompt here" [--verbose]')
+        print('Example: python main.py "How do I fix the calculator?"')
+        sys.exit(1)
+
+    user_prompt = " ".join(args)
+
+    if verbose:
+        print(f"User prompt: {user_prompt}\n")
+
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
-    if len(sys.argv) == 1:
-        print("ERROR: Not giving an argument")
-        sys.exit(1)
-    prompt = sys.argv[1]
+
     messages = [
-        types.Content(role="user", parts=[types.Part(text=prompt)]),
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
+    generate_content(client, messages, verbose)
+
+
+def generate_content(client, messages, verbose):
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
-        config=types.GenerateContentConfig(system_instruction=SYSTEM_PROMPT)
+        config=types.GenerateContentConfig(
+            tools=[available_functions],
+            system_instruction=SYSTEM_PROMPT)
     )
-    print(response.text)
+    if verbose:
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
     
-    if len(sys.argv) == 3 and sys.argv[2] == "--verbose":
-        print(f"User prompt: {prompt}\nPrompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}")
+    if not response.function_calls:
+        return response.text
+    function_responses = []
+    for function_call_part in response.function_calls:
+        function_call_result = call_function(function_call_part, verbose=True)
+        
+        if (
+            not function_call_result.parts
+            or not function_call_result.parts[0].function_response
+        ):
+            raise Exception("Function call result is empty")
+        if verbose:
+            print(f"-> {function_call_result.parts[0].function_response.response}")
+        function_responses.append(function_call_result.parts[0])
+
+    if not function_responses:
+        raise Exception("no function responses generated, exiting.")
+
+
 if __name__ == "__main__":
     main()
